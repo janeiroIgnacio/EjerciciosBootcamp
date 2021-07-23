dc4c8f508744312814a73c8967c6764be09b8716
Ignacio Janeiro <ignacio.janeiro@mercadolibre.com>
2021-07-23 19:34:29 -0300

    Implementacion del print. Solo resta implementarlo con numeros negativos

--- Clase Java 1/src/Practica/Ejercicio6.java
+++ Clase Java 1/src/Practica/Ejercicio6.java
@@ -1,5 +1,7 @@
 package Practica;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio6 extends Ejercicio {
@@ -9,7 +11,7 @@ public class Ejercicio6 extends Ejercicio {
 
         System.out.println("Ejercicio 6: implementacion de RadixSort.");
         int[] iArr = {4,5,32,21,432,3};
-        System.out.println(RadixSort.ordenar(iArr).toString());
+        System.out.println(Arrays.toString(RadixSort.ordenar(iArr)));
 
     }
 }
